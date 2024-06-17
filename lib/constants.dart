d376abf06f7ffe0bd3691eae40f58510c0046a1c
SinghShweta1517 <shweta8462603@gmail.com>
2024-06-17 18:50:58 +0530

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+
+  //Primary color
+
+  static var primaryColor = const Color(0xff284d39);
+  static var blackColour =  Colors.black54;
+
+  //TEXTS
+
+static var titleOne = "Cultivating Beauty in Every Bloom";
+static var descriptionOne = "Discover the secrets to thriving plants—read our care guide now!";
+static var titleTwo = "Cultivate friendships with plant lovers";
+static var descriptionTwo = "Expand your green circle—connect with plant lovers";
+static var titleThree = "Let's green the Earth together";
+static var descriptionThree = "Discover a world of greenery";
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bloom_bliss/ui/onboardingScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       title: "Onboarding Screen",
+      home: OnboardingScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
